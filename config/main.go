af31afdd5573340548db9734f5b1091120b73420
GuiMazur <jjguilh@gmail.com>
2024-01-08 19:04:02 -0300

    fix: config default variables

--- config/main.go
+++ config/main.go
@@ -16,8 +16,8 @@ type Config struct {
 func NewConfig() *Config {
 	return &Config{
 		App{
-			Host: utils.GetEnvOrDefault("APP_HOST", "localhosto"),
-			Port: utils.GetEnvOrDefault("APP_PORT", "8086"),
+			Host: utils.GetEnvOrDefault("APP_HOST", "localhost"),
+			Port: utils.GetEnvOrDefault("APP_PORT", "8080"),
 		},
 	}
 }
